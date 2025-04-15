96d52974134d1529129631bf164548d42866fa23
Ocheje Emmanuel <145170781+ochejeEmmanuel@users.noreply.github.com>
2025-04-15 03:20:18 -0700

    another commit

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-
-// let obj1 = {name: 'sam'}
-// let obj2 = {track: 'web2'}
-// let obj3 = {mode: 'game', firstName : "John", lastName : "Doe", age : 50, eyeColor : "blue"}
-
-// // object.assign
-// // console.log(obj1);
-// // Object.assign(obj2,obj1,obj3)
-// // console.log(obj2);
-
-// // object.assign
-// // let obj4 = Object.create(obj2)
-// // obj4 = {name: 'emma'}
-// // console.log(Object.getPrototypeOf(obj4));
-
-// console.log(Object.keys(obj3));
-// console.log(Object.values(obj3));
-
-// let obj4 = {mode: 'game', firstName : "John", lastName : "Doe", age : 50, eyeColor : "blue"}
-
-// Object.freeze(obj4)
-
-// console.log(obj4);
-// console.log(Object.isFrozen(obj4));
-
 personalInfo = {
     name: "Emma",
     age: 25,
